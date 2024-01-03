b7c028481e6d942943ff41e7ce2e1104fb72b01b
K-R-C <114125671+K-R-C@users.noreply.github.com>
2024-01-03 09:15:27 -0500

    Add files via upload

---
+++ Kyle/spawn_session.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+git clone https://github.com/swson/ADSP.git
+
+cd Kyle/python_files
+
+pip install matplotlib
+
+pip install pandas
+
+python3 new_python_controller.py
+
+cd /data/494_bus/trial_1/single_run/csv_files
