51fc5c0ae04c48e223fef3097d4047f0c0de4ea9
Kyle Chaisson <kyle_chaisson@student.uml.edu>
2024-02-08 23:34:50 -0500

    Updated github project structure to reflect working structure on my machine. Removed old files and directories that were no longer needed.

--- Kyle/data/494_bus/trial_1/single_run/remove_dir_make_dir.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-# use this file during testing to remove faulty data that was stored incorrectly into our csv files directory
-rm -rf csv_files
-
-mkdir csv_files
--- Kyle/misc/print_file_array.py
+++
@@ -1,9 +0,0 @@
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-
-parser.add_argument("list_of_file_names",type = str)
-
-args = parser.parse_args()
-
-print(args.list_of_file_names)
