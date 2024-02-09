dfe3a17710ad5fc13ccfae59e3ce2ffe29e48b38
Kyle Chaisson <kyle_chaisson@student.uml.edu>
2024-02-09 02:22:54 -0500

    converted all of the new corrupted 494_matricies. updated mtx to crs conversion programs

--- Kyle/adsp_project/python_files/multiple_mtx2crs.py
+++ Kyle/adsp_project/python_files/multiple_mtx2crs.py
@@ -2,19 +2,17 @@ import os
 import subprocess
 
 
-matrix_name = "1138_bus"
+matrix_name = "494_bus"
 file_type_original = "mtx"
 file_type_converted = "crs"
 file_composition = "non_zero"
 
 
+os.chdir("..")
+project_home = os.getcwd()
 
 
-custom_path = "/home/kchai/Documents/current/current_research/professor_Son_research/code/project_demo"	# REPLACE THIS WITH FILE PATH TO PROJECT IN YOUR SYSTEM
 
-project_dir_name = "project_dir_template_demo"			
-
-project_home = os.path.join(custom_path,project_dir_name)		# will travel to all directories relative to project home directory
 
 
 
@@ -23,35 +21,31 @@ path_to_downloaded_mtx_files  = os.path.join(project_home,"matrix_directory",mat
 path_to_converted_files = os.path.join(project_home,"matrix_directory",matrix_name,file_type_converted,file_composition )
 
 
-path_to_exe = os.path.join(project_home,"hw4_release","mtx2crs")
-exe_name = "mtx2crs.py"
-
-
-# move conversion program to mtx files directory
-
-os.chdir(project_home)	# this file starts out in the python files directory. need to go to proj home first to get proper scope for directory navigation
-
+path_to_exe = os.path.join(project_home,"python_files")
+exe_name = "multiple_mtx2crs.py"
+exe_name_2 = "mtx2crs.py"
 os.chdir(path_to_exe)
 
 
 
 subprocess.run( ["cp " + exe_name + " " + path_to_downloaded_mtx_files], shell = True )
 
+subprocess.run( ["cp " + exe_name_2 + " " + path_to_downloaded_mtx_files], shell = True )
+
 os.chdir(path_to_downloaded_mtx_files)
+
 
 subprocess.run( ["sudo chmod +x " + exe_name], shell = True)		# want to make program executable in its new context
 
+subprocess.run( ["sudo chmod +x " + exe_name_2], shell = True)		# want to make program executable in its new context
 
 
 print(os.listdir(path_to_downloaded_mtx_files) )
-
-	
-
 print()
 
-for matrix in os.listdir(path_to_downloaded_mtx_files):
+for matrix in sorted(  os.listdir(path_to_downloaded_mtx_files) ):
 	
-	if matrix != exe_name:
+	if (matrix != exe_name) and (matrix != exe_name_2):
 	
 		subprocess.run( [ "sudo ./mtx2crs.py < " + matrix + " > " + path_to_converted_files + "/" + matrix + ".crs"], shell = True )
 		
@@ -61,7 +55,10 @@ for matrix in os.listdir(path_to_downloaded_mtx_files):
 		
 subprocess.run( ["rm " + exe_name ], shell = True )
 
-print(os.listdir(path_to_downloaded_mtx_files) )		
+
+subprocess.run( ["rm " + exe_name_2 ], shell = True )
+
+print(sorted(os.listdir(path_to_downloaded_mtx_files) ))		
 
 
 
